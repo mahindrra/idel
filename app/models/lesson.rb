d586f83ce17d26d05263f527160da6d8dbc16652
mahindrra <mahindrr@gmail.com>
2017-02-14 17:53:46 +0530

    added files type

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,5 +3,10 @@ class Lesson < ApplicationRecord
 
   has_attached_file :content 
   validates_attachment_content_type :content, 
-  :content_type =>['application/pdf','video/x-flv','video/mp4']
+  :content_type =>['application/pdf','video/x-flv','video/mp4',
+  	"application/xlsx", "application/doc", "application/docx",
+   	"application/ppt", "application/pptx",'application/vnd.ms-powerpoint',
+   	'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+   	'application/vnd.oasis.opendocument.presentation'
+   ]
 end
